6e9d0d3c4123d8b90b765da55357eba23a7b4661
hongmaoxiao <769501294@qq.com>
2021-02-26 13:28:40 +0800

    initial commit

---
+++ src/rollup.js
@@ -0,0 +1,17 @@
+import { generate } from 'escodegen';
+import Bundle from './Bundle';
+
+export function rollup ( entry, options = {} ) {
+  const bundle = new Bundle({
+    entry
+  });
+
+  return bundle.collect().then( () => {
+    return {
+      generate: options => bundle.generate( options ),
+      write: () => {
+        throw new Error( 'TODO' );
+      }
+    };
+  });
+}
